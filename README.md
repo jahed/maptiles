ceb3a26e0ddd581b3b1fd69f3fb52ff9e184bf6c
Jahed <jahed.public@gmail.com>
2016-02-03 15:44:08 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,45 @@
+# im-map-tiles
+
+Map Tile Generator for Google Maps, Leaflet and other Map libraries using ImageMagick.
+
+## Dependencies
+
+- ImageMagick
+
+## Usage
+
+```sh
+$ ./im-map-tiles.sh 
+Usage: ./im-map-tiles.sh SOURCE_IMAGE TILES_DESTINATION_DIR [TILE_FORMAT]
+
+SOURCE_IMAGE must exist.
+TILE_FORMAT defaults to 'png'.
+```
+
+Output will take the format of:
+
+```
+${TILES_DESTINATION_DIR}/${ZOOM_LEVEL}/tile_${X}_${Y}.${TILE_FORMAT}
+```
+
+e.g.
+
+```
+./tiles/1/tile_0_1.png
+./tiles/3/tile_2_3.png
+./tiles/5/tile_2_3.png
+...
+```
+
+`${ZOOM_LEVEL}` will start at `0` and go up to the maximum zoom possible for the `${SOURCE_IMAGE}` without enlarging it.
+Each `tile` has a dimension of 256x256 and each ${ZOOM_LEVEL} goes up in dimensions of `2^${ZOOM_LEVEL}` (i.e. 1x1, 2x2, 4x4, 8x8, 16x16 etc.).
+So overall, for each zoom level, the resulting map resolution will be 256x256, 512x512, 1024x1024, 2048x2048 and so on.
+
+If you're using Leaflet, I suggest you set this maximum zoom as your `map.maxNativeZoom` so you can have higher zoom
+levels without the need to download larger, low quality, upscaled tiles.
+
+If a ${ZOOM_LEVEL} folder already exists, it will be skipped.
+
+## License
+
+See `LICENSE` file.
---
+++ im-map-tiles.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+source=$1
+dest_path=$2
+tile_format=$3
+
+if [ ! -f ${source} ] || [ -z ${dest_path} ]; then
+    echo "Usage: ${0} SOURCE_IMAGE TILES_DESTINATION_DIR [TILE_FORMAT]"
+    echo
+    echo "SOURCE_IMAGE must exist."
+    echo "TILE_FORMAT defaults to 'png'."
+    exit 1
+fi
+
+if [ ! -d ${dest_path} ]; then
+    mkdir ${dest_path}
+fi
+
+if [ -z ${tile_format} ]; then
+    tile_format='png'
+fi
+
+original_size=$(identify ${source} | cut -d' ' -f3 | cut -d'x' -f1)
+if [ -z ${original_size} ]; then
+    echo "Could not retrieve image dimensions."
+    exit 1
+fi
+
+tile_size=256
+set -x
+
+for ((zoom=0, resize=0; resize < original_size; zoom++)); do
+    dimensions=$((2 ** ${zoom}))
+	resize=$((${tile_size} * ${dimensions}))
+
+    if [ -d ${dest_path}/${zoom} ]; then
+        echo "Zoom ${zoom} directory already exists. Skipping."
+        continue
+    fi
+
+    echo "Generating tiles for Zoom ${zoom}"
+	mkdir ${dest_path}/${zoom}
+
+	convert ${source} \
+	    -colorspace RGB \
+	    -filter Lanczos2 \
+	    -resize ${resize}x${resize} \
+	    -colorspace sRGB \
+	    -crop ${tile_size}x${tile_size} \
+	    -set filename:tile "%[fx:page.x/${tile_size}]_%[fx:page.y/${tile_size}]"\
+	    +repage \
+        +adjoin \
+        "${dest_path}/${zoom}/tile_%[filename:tile].${tile_format}"
+done
