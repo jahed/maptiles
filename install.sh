58eaeff4d78e899033cce6f76655a77b4ca61991
jahed.dev <3330963+jahed@users.noreply.github.com>
2024-09-10 01:55:37 +0100

    chore: remove install/uninstall scripts

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "Creating symlink"
-ln -s "${PWD}/maptiles" ~/.local/bin/maptiles
-
-echo "Done."
--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "Removing symlink"
-rm ~/.local/bin/maptiles
-
-echo "Done."
